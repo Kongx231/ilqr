8669ceb1cdd6119b160fbfe10d0b92d211d7e69a
Nathan Kong <njkong@andrew.cmu.edu>
2021-10-30 00:57:58 -0400

    Update symbolic_dynamics_floating_base.m

--- matlab/symbolic_dynamics_floating_base.m
+++ matlab/symbolic_dynamics_floating_base.m
@@ -21,21 +21,19 @@ robot_lin_vel = [x_dot;y_dot;z_dot];
 robot_ang_vel_world = [phi_dot_world;theta_dot_world;psi_dot_world];
 states = [robot_pos;robot_orientation;robot_lin_vel;robot_ang_vel_world];
 
+% Define the position of the feet
 foot_pos_1 = [foot_pos_1x foot_pos_1y foot_pos_1z]';
 foot_pos_2 = [foot_pos_2x foot_pos_2y foot_pos_2z]';
 foot_pos_3 = [foot_pos_3x foot_pos_3y foot_pos_3z]';
 foot_pos_4 = [foot_pos_4x foot_pos_4y foot_pos_4z]';
 
-% hip_pos_1 = robot_pos + hip_offset_1;
-% hip_pos_2 = robot_pos + hip_offset_2;
-% hip_pos_3 = robot_pos + hip_offset_3;
-% hip_pos_4 = robot_pos + hip_offset_4;
-
+% Get the difference between the robot position and feet position
 r_1 = robot_pos - foot_pos_1;
 r_2 = robot_pos - foot_pos_2;
 r_3 = robot_pos - foot_pos_3;
 r_4 = robot_pos - foot_pos_4;
 
+% Define the forces
 F_1 = [F_1x F_1y F_1z]';
 F_2 = [F_2x F_2y F_2z]';
 F_3 = [F_3x F_3y F_3z]';
@@ -43,6 +41,7 @@ F_4 = [F_4x F_4y F_4z]';
 
 inputs = [F_1;F_2;F_3;F_4];
 
+% Create rpy rotation matrix
 rot_roll = [1,0,0;
             0,cos(phi),-sin(phi);
             0,sin(phi),cos(phi)];
@@ -53,26 +52,28 @@ rot_yaw = [cos(psi),-sin(psi),0;
             sin(psi),cos(psi),0;
             0, 0, 1];
 rot_mat = rot_yaw*rot_pitch*rot_roll;
+
+% Define the body inertia and then map it to world inertia (eq 14)
 I = diag([I_xx,I_yy,I_zz]);
 I_hat = rot_mat'*I*rot_mat;
 I_hat_inv = simplify(inv(I_hat));
 
-% Map body to world angular velocities
+% Map body to world angular velocities (eq 10)
 rot_mat_vel = [cos(theta)*cos(psi),-sin(psi),0;
                 cos(theta)*sin(psi),cos(psi),0;
                 0,0,1];
             
 ang_vel_body = simplify(inv(rot_mat_vel)*robot_ang_vel_world);
-% Get angular acceleration from the wrenches
+% Get angular acceleration from the wrenches (we use the same approximation
+% as eq 13) (eq 6)
 ang_accel_world = simplify(I_hat_inv*(skew(r_1)*F_1 + skew(r_2)*F_2 + skew(r_3)*F_3 + skew(r_4)*F_4));
 
-% Get linear accleartion from the forces
+% Get linear accleartion from the forces (eq 5)
 gravity_vec = [0;0;g];
 
 linear_accel = 1/m*(F_1+F_2+F_3+F_4)+gravity_vec;
 
-% Ordering, position, ang position, linear vel, angular vel
-
+% Ordering of states, position, ang position, linear vel, angular vel
 f = [robot_lin_vel;ang_vel_body;linear_accel;ang_accel_world];
 
 % Eueler integration to define discrete update
